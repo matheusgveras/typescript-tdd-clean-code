03777f4b1b9e8758174d19584ff36d855d64461b
matheusveras <matheus.veras@lobofrontal.com.br>
2022-01-24 10:55:48 -0300

    chore: configurando o sass

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,22 @@ module.exports = {
         filename: 'bundle.js'
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
+        extensions: ['.ts', '.tsx', '.js', 'scss'],
         alias: {
             '@': path.join(__dirname, 'src')
         }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            }, {
+                test: /\.scss$/
+            }
+        ]
+       
     },
     devServer: {
         contentBase: './public',
